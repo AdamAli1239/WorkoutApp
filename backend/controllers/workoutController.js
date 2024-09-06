9bc793221ec00acdab9755e1030709a758d247d4
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-09-06 10:20:52 -0400

    havent created the logic for the controllers yet.

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,17 @@
+const getWorkouts = async (req, res) => {};
+
+const getWorkout = async (req, res) => {};
+
+const updateWorkout = async (req, res) => {};
+
+const createWorkout = async (req, res) => {};
+
+const deleteWorkout = async (req, res) => {};
+
+module.exports = {
+  getWorkouts,
+  getWorkout,
+  updateWorkout,
+  createWorkout,
+  deleteWorkout,
+};
