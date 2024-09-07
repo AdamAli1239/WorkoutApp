54ba79c2d85ca81d90425e717e82b093041935cf
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-09-07 11:54:49 -0400

    completed the workout context

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,7 +1,30 @@
 import { createContext, useReducer } from "react";
 
-export const WorkoutReducer = (state,action) => {
-
+export const WorkoutReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return { workouts: action.payload };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [...state.workouts, action.payload],
+      };
+    case "UPDATE_WORKOUT":
+      return {
+        workouts: [...state.workouts, action.payload],
+      };
+    case "DELETE_WORKOUT":
+      return {
+        workouts: [
+          ...state.workouts.filter(
+            (workout) => workout.id !== action.payload.id
+          ),
+        ],
+      };
+    default:
+      return {
+        workouts: state,
+      };
+  }
 };
 
 export const WorkoutContext = createContext();
