5dad1eba1947442b4854cb9684708de7e6be8640
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-09-08 12:19:38 -0400

    created a template to displa the workout details

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,5 +1,30 @@
-const WorkoutDetails = () => {
-    return (  );
-}
- 
+import { useWorkoutHook } from "../hooks/useWorkoutContext";
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutHook();
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await response.json();
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+  return (
+    <div>
+      <h3>{workout.title}</h3>
+      <p>{workout.reps}</p>
+      <p>{workout.weight}</p>
+
+      <span onClick={handleClick} className="hover:red">
+        Click to delete the workout
+      </span>
+    </div>
+  );
+};
+
 export default WorkoutDetails;
